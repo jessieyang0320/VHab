2a58f464085733fb92a2ba2d39297af66b51d793
faralbrt <albert.farhi5@gmail.com>
2017-01-17 12:00:30 -0500

    output count and addTrap()

--- js/level-1/whackamole.js
+++ js/level-1/whackamole.js
@@ -6,17 +6,21 @@ function Counter() {
 
 var counter = new Counter();
 
-
+var yPositions = [0.1, 0.2, 0.3]
+var xPositions = [-0.17, -0.14, -0.11, -0.08, -0.05, -0.02, 0.01, 0.05, 0.07, 0.11, 0.15]
 
 // Circular button:
 var gameCallBack = function(mesh){
   mesh.material.color.setHex(0xccccff);
   counter.count += 1;
+  $("#hit_count").html(counter.count)
   scene.remove(this.plane.mesh)
+  addWhackamole(gameCallBack)
 }
 
 // Add Buttons to the scene
 addWhackamole(gameCallBack)
+addTrap(gameCallBack)
 
 // ranges:
 // X: -0.18 - 0.15
@@ -33,11 +37,12 @@ function addWhackamole(callBackFunction) {
 
   var circleGeo = new THREE.CircleGeometry(0.02, 32);
   var buttonMesh = new THREE.Mesh(circleGeo, material.clone());
-  buttonMesh.name = "round button";
-  var px = parseFloat((Math.random() * (0.15 + 0.18) - 0.18).toFixed(2))
-  var py = parseFloat((Math.random() * (0.3 - 0.1) + 0.1).toFixed(2))
+  buttonMesh.name = "mole";
+  // var px = parseFloat((Math.random() * (0.15 + 0.18) - 0.18).toFixed(2))
+  // var py = parseFloat((Math.random() * (0.3 - 0.1) + 0.1).toFixed(2))
+  var px = sample(xPositions)
+  var py = sample(yPositions)
   var pz = -0.05
-
   buttonMesh.position.set(px, py, pz);
   scene.add(buttonMesh);
   var roundButton = new PushButton(
@@ -46,4 +51,27 @@ function addWhackamole(callBackFunction) {
   ).on('release', function(mesh){
     mesh.material.color.setHex(0xd81a0d);
   });
+}
+
+function addTrap(callBackFunction)  {
+  var circleGeo = new THREE.CircleGeometry(0.02, 32);
+  var buttonMesh = new THREE.Mesh(circleGeo, material.clone());
+  buttonMesh.name = "mole";
+  // var px = parseFloat((Math.random() * (0.15 + 0.18) - 0.18).toFixed(2))
+  // var py = parseFloat((Math.random() * (0.3 - 0.1) + 0.1).toFixed(2))
+  var px = sample(xPositions)
+  var py = sample(yPositions)
+  var pz = -0.05
+
+  buttonMesh.material.color.setHex(0x223047)
+  buttonMesh.position.set(px, py, pz);
+  scene.add(buttonMesh);
+  var roundButton = new PushButton(
+    new InteractablePlane(buttonMesh, Leap.loopController, {moveX: false, moveY: false})
+  ).on('press', callBackFunction)
+}
+
+function sample(arr) {
+  var index = Math.floor(Math.random() * arr.length);
+  return arr[index];
 }
