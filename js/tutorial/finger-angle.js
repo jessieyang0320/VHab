e5f822d1bf3d3d06cdc69864d071bcc3da483114
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-01-17 15:25:45 -0500

    add counter to finger-angle

--- js/tutorial/finger-angle.js
+++ js/tutorial/finger-angle.js
@@ -1,3 +1,9 @@
+function Counter() {
+  this.count = 0
+}
+
+var counter = new Counter();
+
 Leap.loop({background: true}, {
 
   hand: function(hand){
@@ -7,9 +13,18 @@ Leap.loop({background: true}, {
     );
 
     var angle = Math.acos(dotProduct);
-    if (angle > .2 && finished.status === false){
-      moveOn("You did it! You can move on.", 'grab-strength.html')
-      finished.status = true;
+    if (angle > .4 && finished.status === false){
+// 0.2 -> 15 degree  0.5->30 degree
+      console.log(counter.count)
+      counter.count +=1
+      
+      sleep(800)
+
+      if(checkSuccess()){    
+      moveOn("You did it!! You can move on.", 'finger-angle.html')
+      finished.status = true
+      }
+      
     }
 
     progress.style.width = angle * 100 + '%';
@@ -23,5 +38,20 @@ Leap.loop({background: true}, {
         rendererOps: {antialias: true}
       })
       .use('proximity');
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+function checkSuccess() {
+  if(counter.count === 4) {
+    return true;
+  }
+}
 
   var progress = document.getElementById('progress');
