0ff05c61355f6123b1e624d8dc7e48e9d92c6693
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-01-17 14:11:22 -0500

    add counter to finger sthrength

--- js/tutorial/pinch-strength.js
+++ js/tutorial/pinch-strength.js
@@ -1,9 +1,23 @@
+function Counter() {
+  this.count = 0
+}
+
+var counter = new Counter();
+
 Leap.loop({background: true}, {
 
   hand: function(hand){
-  if (hand.pinchStrength > .8 && finished.status === false){
-    moveOn("You did it!! You can move on.", 'finger-angle.html')
-    finished.status = true
+      
+  if (hand.pinchStrength > 0.8 && finished.status === false){
+     counter.count +=1
+     console.log("hello")
+     sleep(800)
+
+    if(checkSuccess()){    
+      moveOn("You did it!! You can move on.", 'finger-angle.html')
+      finished.status = true
+    }
+
   }
   progress.style.width = hand.pinchStrength * 100 + '%';
 
@@ -15,5 +29,21 @@ Leap.loop({background: true}, {
       })
       .use('proximity');
 
+
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+function checkSuccess() {
+  if(counter.count === 4) {
+    return true;
+  }
+}
 
 var progress = document.getElementById('progress');
