10b53bcd1abc5960afc3ae11ffa85571153c364c
Natalie_Liles <Natalieliles@gmail.com>
2017-01-18 13:40:58 -0500

    Fix merge conflicts and changed the link on buttons.

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,19 +1,19 @@
-addButton(-0.2, 0.1, -0.05, function(mesh){
-  window.location.href = 'views/buttons.html'
+addButton(-0.2, 0.1, -0.05, "Pinch", 110 , 280, function(mesh){
+  window.location.href = 'views/pinch-strenght.html'
 })
 
-addButton(-0.1, 0.1, -0.05, function(mesh) {
-  window.location.href = 'views/pinch-strength.html'
-})
-
-addButton(0, 0.1, -0.05, function(mesh){
+addButton(-0.1, 0.1, -0.05, "Finger Angle", 70 , 280, function(mesh) {
   window.location.href = 'views/finger-angle.html'
 })
 
-addButton(0.1, 0.1, -0.05, function(mesh){
+addButton(0, 0.1, -0.05, "Grab", 70 , 280, function(mesh){
   window.location.href = 'views/grab-strength.html'
 })
 
-addButton(0.2, 0.1, -0.05, function(mesh){
+addButton(0.1, 0.1, -0.05, "Whackamole", 70 , 280, function(mesh){
   window.location.href = 'views/whackamole.html'
+})
+
+addButton(0.2, 0.1, -0.05, "Pinch Selection", 70 , 280, function(mesh){
+  window.location.href = 'views/pinch-selector.html'
 })
