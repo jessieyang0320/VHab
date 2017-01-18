efbd20fd636e2f72b0793772011fe2c105923cea
Jenna Van Conett <jenvanc@gmail.com>
2017-01-18 12:00:25 -0500

    Refacor buttons

---
+++ js/controllers/grab-strength.js
@@ -0,0 +1,43 @@
+function Counter() {
+  this.count = 5
+}
+
+var counter = new Counter();
+var finished = new Finished();
+
+Leap.loop({background: true}, {
+
+  hand: function(hand){
+  if (hand.grabStrength > .8 && finished.status === false){
+    counter.count -= 1
+    console.log(counter.count)
+    sleep(790)
+    if (counter.count === 0) {
+    moveOn("You did it!! You can move on.", '../index.html')
+    finished.status = true    
+    }
+
+  }
+  output.innerHTML = hand.grabStrength.toPrecision(2);
+  progress.style.width = hand.grabStrength * 100 + '%';
+
+}
+}).use('boneHand', {
+        targetEl: document.body,
+        jointColor: new THREE.Color(0xffffff),
+        rendererOps: {antialias: true}
+      })
+      .use('proximity');
+
+
+var progress = document.getElementById('progress');
+    output = document.getElementById('output');
+
+  function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
---
+++ js/helpers/scene.js
@@ -0,0 +1,15 @@
+Leap.loop({background: true})
+  .use('transform', {
+    vr: 'desktop'
+  })
+  .use('boneHand', {
+    targetEl: document.body,
+    jointColor: new THREE.Color(0xffffff),
+    rendererOps: {antialias: true}
+  })
+  .use('proximity');
+
+var scene = Leap.loopController.plugins.boneHand.scene;
+var camera = Leap.loopController.plugins.boneHand.camera;
+var renderer = Leap.loopController.plugins.boneHand.renderer;
+camera.position.set( 0, 0.3, 0.6 );
