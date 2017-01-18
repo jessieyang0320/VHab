3098df24341a8dd4af5586aad76b8831091cfa1e
Natalie_Liles <Natalieliles@gmail.com>
2017-01-18 12:11:48 -0500

    Change button font size and color. Needs to be fixed.

--- js/application.js
+++ js/application.js
@@ -17,10 +17,10 @@ function addButton(px, py, pz, name, p1, p2, callBackFunction) {
 
   var dynamicTexture  = new THREEx.DynamicTexture(512,512)
   dynamicTexture.texture.needsUpdate  = true
-  dynamicTexture.context.font = "60px Arial";
+  dynamicTexture.context.font = "110px Arial";
   dynamicTexture.texture.anisotropy = renderer.getMaxAnisotropy()  
-  dynamicTexture.clear('#E6E6FA')
-  texture1 = dynamicTexture.drawText(name, p1, p2, 'black')
+  dynamicTexture.clear('#43C59E')
+  texture = dynamicTexture.drawText(name, p1, p2, 'black')
   var material    = new THREE.MeshBasicMaterial({      
       map : texture.texture,
       color: 0xffffff, 
@@ -28,9 +28,6 @@ function addButton(px, py, pz, name, p1, p2, callBackFunction) {
   })
   var circleGeo = new THREE.CircleGeometry(0.04, 32);
   var buttonMesh = new THREE.Mesh(circleGeo, material);
-  var buttonMesh = new THREE.Mesh(circleGeo, material2);
-  var buttonMesh = new THREE.Mesh(circleGeo, material3);
-  var buttonMesh = new THREE.Mesh(circleGeo, material2);
   buttonMesh.name = "round button";
   buttonMesh.position.set(px, py, pz);
   scene.add(buttonMesh);
--- js/tutorial/index.js
+++ js/tutorial/index.js
@@ -1,13 +1,12 @@
-addButton(-0.2, 0.1, -0.05, "Pinch Strenth", 70 , 280, function(mesh){
+addButton(-0.2, 0.1, -0.05, "Pinch", 110 , 280, function(mesh){
   window.location.href = 'tutorial/buttons.html'
 })
 
 addButton(-0.1, 0.1, -0.05, "Finger Angle", 70 , 280, function(mesh) {
-  mesh.texture.dynamicTexture.drawText('Pinch Strength', 70, 280, 'black')
   window.location.href = 'tutorial/pinch-strength.html'
 })
 
-addButton(0, 0.1, -0.05, "Grab Strength", 70 , 280, function(mesh){
+addButton(0, 0.1, -0.05, "Grab", 70 , 280, function(mesh){
   window.location.href = 'tutorial/finger-angle.html'
 })
 
