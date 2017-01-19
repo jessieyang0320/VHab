19c332b7f723db7ddbae3816740784355093fc5e
faralbrt <albert.farhi5@gmail.com>
2017-01-19 11:17:43 -0500

    fix status bar on pinch strength

--- js/controllers/pinch-strength.js
+++ js/controllers/pinch-strength.js
@@ -1,9 +1,9 @@
 function Counter() {
-  this.count = 0
+  this.count = 0;
 }
 
 var pinchStrength , oPinchStrength;
-var pinchStrengthCutoff = .8;
+var pinchStrengthCutoff = 0.8;
 var counter = new Counter();
 camera.position.set( 0, 0.3, 0.4 );
 
@@ -13,9 +13,9 @@ Leap.loop({background: true}, {
     oPinchStrength = pinchStrength;
     pinchStrength = hand.pinchStrength;
   if (pinchStrength > pinchStrengthCutoff && oPinchStrength <= pinchStrengthCutoff && finished.status === false){
-     counter.count +=1
+     counter.count +=1;
      $("#pinch_sound")[0].play();
-     console.log(counter.count)
+     console.log(counter.count);
 
   if (counter.count===2){
       $("#doing-well")[0].play();
@@ -24,16 +24,16 @@ Leap.loop({background: true}, {
   if (counter.count===4){
       $("#you_are_so_close")[0].play();
   }
-    
+
   if(checkSuccess()){
     $("#you-did-it")[0].play();
-    moveOn("You did it!! You can move on.", '../index.html')
-    finished.status = true
+    moveOn("You did it!! You can move on.", '../index.html');
+    finished.status = true;
   }
-  count.innerHTML = 'Count: ' + counter.count
-  output.innerHTML = hand.pinchStrength.toPrecision(2);
-  progress.style.width = hand.pinchStrength * 100 + '%';
 }
+count.innerHTML = 'Count: ' + counter.count;
+output.innerHTML = hand.pinchStrength.toPrecision(2);
+progress.style.width = hand.pinchStrength * 100 + '%';
 }
 
 }).use('boneHand', {
